3fcccec913668e7c71e3d79f87756a34400fecbd
BreynerPavas <71611701+BreynerPavas@users.noreply.github.com>
2025-01-23 09:34:01 +0100

    Update Padre.js

--- models/Padre.js
+++ models/Padre.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.SchemaTypes.ObjectId;
 
 const PadreSchema = new mongoose.Schema({
+  tipoId: { type: ObjectId, ref: "Tipo" }, // 2
   fotosIds: [{ type: ObjectId, ref: "Foto" }],
-  tipoId: { type: ObjectId, ref: "Tipo" },
-  fotoseta: String,
+  etiqueta: String,
   loteId: { type: ObjectId, ref: "Lote" },  
   master: [], // dos fotos de amberso y reverso del album puede ser null
 }, { timestamps: true });
