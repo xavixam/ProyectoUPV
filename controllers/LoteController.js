fd30df8d3244f2c6da4cfb0d0f73fc8cbe41ce14
BreynerPavas <71611701+BreynerPavas@users.noreply.github.com>
2025-01-23 11:54:27 +0100

    getByIdPopulate

--- controllers/LoteController.js
+++ controllers/LoteController.js
@@ -1,4 +1,6 @@
 const Lote = require("../models/Lote")
+const SubCarpeta = require('../models/SubCarpeta');
+const SubCarpetaInterna = require('../models/SubCarpetaInterna');
 
 const LoteController = {
     
@@ -22,13 +24,29 @@ const LoteController = {
     },
     async getById(req, res) {
         try {
-            const lote = await Lote.findById(req.params._id)
-            .populate("contenidoIds")
-            res.send(lote)
+            const { id } = req.params;
+    
+            // Buscar el Lote por su ID y hacer populate de las subCarpetas y subCarpetasInternas
+            const lote = await Lote.findById(id)
+                .populate({
+                    path: 'subCarpetas', // Popular el array de subCarpetas
+                    populate: {
+                        path: 'subCarpetasInternas', // Popular las subCarpetasInternas dentro de cada subCarpeta
+                        model: 'SubCarpetaInterna', // Asegúrate de que el modelo esté correctamente definido
+                    },
+                });
+    
+            // Si no se encuentra el lote, devolvemos un 404
+            if (!lote) {
+                return res.status(404).send({ message: "Lote no encontrado" });
+            }
+    
+            res.status(200).send({ message: "Lote encontrado", lote });
         } catch (error) {
             console.error(error);
+            res.status(500).send({ message: "Ha habido un problema", error });
         }
-    },
+    }
 }
 
 module.exports = LoteController
