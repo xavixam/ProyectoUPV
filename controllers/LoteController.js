3cd053310df3a5001eda40e0987b8cec54c65def
unknown <joan_mart_her@hotmail.com>
2025-01-29 11:42:42 +0100

    Cambios pull

--- controllers/LoteController.js
+++ controllers/LoteController.js
@@ -24,46 +24,6 @@ const LoteController = {
       console.error(error);
     }
   },
-  // async getById(req, res) {
-  //     try {
-  //         const { _id } = req.params;
-
-  //         // Buscar el Lote por su ID y hacer populate de las subCarpetas y subCarpetasInternas
-  //         const lote = await Lote.findById(_id);
-
-  //         // Si no se encuentra el lote, devolvemos un 404
-  //         if (!lote) {
-  //             return res.status(404).send({ message: "Lote no encontrado" });
-  //         }
-  //         const loteObject = lote.toObject();
-  //         // Enriquecer la información de las subcarpetas con los datos de SubCarpeta
-
-  //         const infoSubCarpetas = await Promise.all(
-  //             lote.subCarpetas.map(async (subcarpetaId) => {
-  //                 // Buscar cada subcarpeta por su id_subcarpeta
-  //                 const subcarpeta = await SubCarpeta.findOne({ id_subcarpeta: subcarpetaId });
-  //                 if (subcarpeta) {
-  //                     return {
-  //                         id_subcarpeta: subcarpeta.id_subcarpeta,
-  //                         nRegistro: subcarpeta.nRegistro,
-  //                         tipo: subcarpeta.tipo,
-  //                         subCarpetasInternas: subcarpeta.subCarpetasInternas,
-  //                     };
-  //                 } else {
-  //                     return { id_subcarpeta: subcarpetaId, message: "Subcarpeta no encontrada" };
-  //                 }
-  //             })
-  //         );
-
-  //         // Añadir la información de las subcarpetas al lote
-  //         loteObject.infoSubCarpetas = infoSubCarpetas;
-
-  //     res.status(200).send({ message: "Lote encontrado", lote: loteObject });
-  //     } catch (error) {
-  //         console.error(error);
-  //         res.status(500).send({ message: "Ha habido un problema", error });
-  //     }
-  // },
   async getById(req, res) {
     try {
       const { _id } = req.params;
