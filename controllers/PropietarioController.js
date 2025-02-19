19456cb39e64c9d8419572275725619d58dce18d
xavi <antunez.xavi.grao@gmail.com>
2025-02-19 12:42:06 +0100

    getAll y create propietario

--- controllers/PropietarioController.js
+++ controllers/PropietarioController.js
@@ -4,10 +4,20 @@ const PropietarioController = {
   async getAll(req, res) {
     try {
       const propietario = await Propietario.find()
-      .populate("faseId")
+        .populate("loteId")
       res.send(propietario);
     } catch (error) {
       console.error(error);
+    }
+  },
+  async create(req, res) {
+    try {
+      const propietario = await Propietario.create(req.body)
+
+      res.status(201).send({ message: "New person successfully created", propietario })
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "There was a problem", error })
     }
   },
 };
--- routes/propietario.js
+++ routes/propietario.js
@@ -4,5 +4,6 @@ const PropietarioController = require("../controllers/PropietarioController")
 const router = express.Router()
 
 router.get("/", PropietarioController.getAll)
+router.post("/", PropietarioController.create)
 
 module.exports = router
